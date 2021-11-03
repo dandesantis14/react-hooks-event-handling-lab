777086d37b83725a678acc4a44382f8652752122
Daniel <dandesantis14@gmail.com>
2021-11-03 18:42:06 -0400

    commit tree 40bd5517fd29cbaae7d4a9175bb719ef546bbeeb

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+import React from 'react';
+
+function EyesOnMe(){
+    function focused () {
+        console.log('Good!')
+    }
+    function outOfFocus (){
+        console.log('Hey! Eyes on me!')
+    }
+    return( 
+        <button onFocus={focused} onBlur={outOfFocus}>Eyes on me</button>
+    );
+};
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react';
+
+function Keypad(){
+    function keypadChange (e){
+        console.log('Entering password...')
+    }
+    return( 
+        <input type="password" onChange={keypadChange} ></input>
+    );
+};
+export default Keypad
